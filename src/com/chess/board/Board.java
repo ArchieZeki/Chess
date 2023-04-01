dac42b9c718d3cbbeb5d3bbdf900d5a60782369e
ArchieZeki <ArchieZeki@gmail.com>
2023-04-01 20:36:25 +0100

    initial commit

---
+++ src/com/chess/board/Board.java
@@ -0,0 +1,33 @@
+package com.chess.board;
+
+import com.chess.commons.File;
+import com.chess.commons.Location;
+import com.chess.squares.Square;
+import com.chess.squares.SquareColour;
+
+public class Board {
+    Square[][] boardSquares = new Square[8][8];
+
+    public Board() {
+        for(int i = 0; i <boardSquares.length; i++) {
+            int column = 0;
+            SquareColour currentColour = (i % 2 == 0) ? SquareColour.DARK : SquareColour.LIGHT;
+            for (File file : File.values()) {
+                Square newSquare = new Square(currentColour, new Location(file, i));
+                boardSquares[i][column] = newSquare;
+                currentColour = (currentColour == SquareColour.LIGHT) ? SquareColour.DARK : SquareColour.LIGHT;
+                column++;
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (Square[] row : boardSquares) {
+            for (Square square : row) {
+                System.out.print(square);
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/com/chess/squares/Square.java
@@ -0,0 +1,44 @@
+package com.chess.squares;
+
+import com.chess.commons.Location;
+
+public class Square {
+    private final SquareColour squareColour;
+    private final Location location;
+    private boolean isOccupied;
+
+    public Square(SquareColour squareColour, Location location) {
+        this.squareColour = squareColour;
+        this.location = location;
+        this.isOccupied = false;
+    }
+
+    public void reset() {
+        this.isOccupied = false;
+    }
+
+    public SquareColour getSquareColour() {
+        return squareColour;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "squareColour=" + squareColour +
+                ", location=" + location +
+                ", isOccupied=" + isOccupied +
+                '}';
+    }
+}
